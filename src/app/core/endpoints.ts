68443b62ef9bb9c4f16e3099f2ebc91cc35899b5
Predrag <predragzivkovic@elfak.rs>
2026-02-03 23:38:34 +0100

    endpoints

--- src/app/core/endpoints.ts
+++ src/app/core/endpoints.ts
@@ -1,6 +1,6 @@
 export class EndPoint{
-    public static readonly login='http://localhost:3000/auth/login';
-    public static readonly register='http://localhost:3000/user/createUser';
-    public static readonly userEndpoints='http://localhost:3000/user/';
-    public static readonly taskEndpoints='http://localhost:3000/task/';
+    public static readonly login='https://project-nest-a7e2ebeghag5bvhp.centralus-01.azurewebsites.net/auth/login';
+    public static readonly register='https://project-nest-a7e2ebeghag5bvhp.centralus-01.azurewebsites.net/user/createUser';
+    public static readonly userEndpoints='https://project-nest-a7e2ebeghag5bvhp.centralus-01.azurewebsites.net/user/';
+    public static readonly taskEndpoints='https://project-nest-a7e2ebeghag5bvhp.centralus-01.azurewebsites.net/task/';
 }
